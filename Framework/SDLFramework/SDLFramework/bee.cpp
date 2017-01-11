dc5f45a1c3fc39c6e607fbfa57ca367acbf7aa19
bramvdzee <bramvdzee1@gmail.com>
2017-01-11 12:56:22 +0100

    Eh

--- Framework/SDLFramework/SDLFramework/bee.cpp
+++ Framework/SDLFramework/SDLFramework/bee.cpp
@@ -2,9 +2,18 @@
 
 Bee::Bee()
 {
+<<<<<<< HEAD
 	this->texture = mApplication->LoadTexture("bee.png");
 	this->SetTexture(texture);
 	this->SetSize(25, 21);
+=======
+	_color = color;
+	this->SetTexture(img);
+	this->mApplication = app;
+	this->mHeight = 50;
+	this->mWidth = 50;
+	app->AddRenderable(this);
+>>>>>>> parent of fb4881e... size bee
 }
 
 
